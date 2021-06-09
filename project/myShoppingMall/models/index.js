1ee35620a2d2aea79890b35af6263a7f71d12ab5
강재현 <wol5971@gmail.com>
2021-06-09 10:25:25 +0000

    commit

--- project/myShoppingMall/models/index.js
+++ project/myShoppingMall/models/index.js
@@ -1,10 +1,14 @@
 const Sequelize=require('sequelize');
-const env=process.env.NODE||"development";
-const config=require('../config/config')[env];
 const User=require('./user');
 const Product=require('./product');
 const Comment=require('./comment');
 const Basket=require('./basket');
+const Hashtag=require('./hashtag');
+const Buy=require('./buy');
+
+
+const env=process.env.NODE_ENV||"development";
+const config=require('../config/config')[env];
 
 const db={};
 const sequelize=new Sequelize(
@@ -16,15 +20,21 @@ db.User=User;
 db.Product=Product;
 db.Comment=Comment;
 db.Basket=Basket;
+db.Hashtag=Hashtag;
+db.Buy=Buy;
 
 User.init(sequelize);
 Product.init(sequelize);
 Comment.init(sequelize);
 Basket.init(sequelize);
+Hashtag.init(sequelize);
+Buy.init(sequelize);
 
 User.associate(db);
 Product.associate(db);
 Comment.associate(db);
 Basket.associate(db);
+Hashtag.associate(db);
+Buy.associate(db);
 
 module.exports=db;
